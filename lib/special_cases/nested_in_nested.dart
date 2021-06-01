04784a81fbdce5c39a2eed14c85ab983b23be1d2
Schaban <s.bochi@qlevar.com>
2021-06-01 20:20:14 +0200

    Add nested in nested example

---
+++ lib/special_cases/nested_in_nested.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:qlevar_router/qlevar_router.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final routes = [
+    QRoute(path: '/', builder: () => WelcomePage()),
+    QRoute.withChild(
+        path: 'nested-1',
+        builderChild: (r) => Nested1(r),
+        children: [
+          QRoute(path: '/', builder: () => WelcomePage()),
+          QRoute(path: '/1', builder: () => WelcomePage(index: 1)),
+          QRoute(path: '/2', builder: () => WelcomePage(index: 2)),
+          QRoute.withChild(
+              path: 'nested-2',
+              builderChild: (r) => Nested2(r),
+              children: [
+                QRoute(path: '/', builder: () => WelcomePage()),
+                QRoute(path: '/3', builder: () => WelcomePage(index: 3)),
+                QRoute(path: '/4', builder: () => WelcomePage(index: 4)),
+              ]),
+        ])
+  ];
+  @override
+  Widget build(BuildContext context) => MaterialApp.router(
+      routeInformationParser: const QRouteInformationParser(),
+      routerDelegate: QRouterDelegate(routes));
+}
+
+class WelcomePage extends StatelessWidget {
+  final int? index;
+  WelcomePage({this.index});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Card(child: Text(index == null ? 'welcome' : index.toString())),
+      ),
+      floatingActionButton: QR.currentPath == '/'
+          ? TextButton(
+              child: Text('Go to nested-1'),
+              onPressed: () => QR.to('/nested-1'),
+            )
+          : TextButton(
+              child: Text('Go to Home'),
+              onPressed: () => QR.to('/'),
+            ),
+    );
+  }
+}
+
+class Nested1 extends StatelessWidget {
+  final QRouter router;
+  Nested1(this.router);
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+                onPressed: () => QR.to('nested-1/1'), child: Text('Go to 1')),
+            TextButton(
+                onPressed: () => QR.to('nested-1/2'), child: Text('Go to 2')),
+            TextButton(
+                onPressed: () => QR.to('nested-1/nested-2'),
+                child: Text('Go to nested-2')),
+            Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.amber)),
+              width: size.width * 0.7,
+              height: size.height * 0.7,
+              child: router,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Nested2 extends StatelessWidget {
+  final QRouter router;
+  Nested2(this.router);
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+                onPressed: () => QR.to('nested-1/nested-2/3'),
+                child: Text('Go to 3')),
+            TextButton(
+                onPressed: () => QR.to('nested-1/nested-2/4'),
+                child: Text('Go to 4')),
+            Container(
+              width: size.width * 0.5,
+              height: size.height * 0.5,
+              child: router,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
